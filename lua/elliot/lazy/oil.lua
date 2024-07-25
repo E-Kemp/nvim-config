f51e4f2d4b8a5b9711b52a6b1393fb5af7ae7624
Elliot Kemp <35563838+E-Kemp@users.noreply.github.com>
2024-07-25 11:16:26 +0100

    Added Whichkey aswell

--- lua/elliot/lazy/oil.lua
+++ lua/elliot/lazy/oil.lua
@@ -7,6 +7,7 @@ return {
         show_hidden = true,
       },
     })
-    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory (old)" })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end
 }
--- lua/elliot/remap.lua
+++ lua/elliot/remap.lua
@@ -1,1 +1,3 @@
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
